07a5c6e4dad91d7c9698a7842e40aa0e5f7260c5
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-07-24 15:24:21 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+const num1=2
+const num2 = 31
+const multiply = num1*num2
+const random = Math.floor(Math.random() *10)+0
+const num3 =7
+const num4 = 60
+const mod = num4%num3
+const max = Math.max(2,20,15)
